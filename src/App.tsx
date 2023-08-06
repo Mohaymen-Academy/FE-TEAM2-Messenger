b6ab95572a32023157c8ba9835c62e9838b7df5b
mostafagh79 <mostafa.gh.79a@gmail.com>
2023-08-06 16:14:30 +0330

    Basic layout of sign up and login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-
 import { Route, Routes } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ThemeToggle from "./components/ThemeToggle";
-import Paragraph from "./components/ui/paragraph/Paragraph";
 import Chat from "./pages/chat/Chat";
 import Auth from "./pages/auth/Auth";
 import SignUp from "./components/Login & SignUp/SignUp";
+import Login from "./components/Login & SignUp/Login";
 
 
 function App() {
@@ -17,7 +16,7 @@ function App() {
       <Routes >
         <Route path="/Chat" element={<Chat />}/>
         <Route path="/Auth" element={<Auth/>}>
-          <Route path="Login" element={<p>11</p>} />
+          <Route path="Login" element={<Login/>} />
           <Route path="SignUp" element={<SignUp/>} />
         </Route>
       </Routes>
