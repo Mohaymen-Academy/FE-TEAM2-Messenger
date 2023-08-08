65937ee7dedcbef1e46f96cb2dda1e5ae84e1ae8
abolfazl omidvar <abolfazl.omidvar46@gmail.com>
2023-08-08 09:28:59 +0330

    fix: desktop sidebar padding

--- src/components/desktopSideBar/DesktopSidebar.tsx
+++ src/components/desktopSideBar/DesktopSidebar.tsx
@@ -9,22 +9,23 @@ import {
 } from "react-icons/bs";
 import { MdLogout } from "react-icons/md";
 import { FiSettings } from "react-icons/fi";
-import { AiFillCaretLeft } from "react-icons/ai";
+import { AiFillCaretRight } from "react-icons/ai";
 
 const DesktopSidebar = () => {
   const [show, setShow] = useState(true);
   return (
     <div
+      style={{ padding: show ? "0.5rem" : "0rem" }}
       className={clsx(
-        "p-2 border-l dark:border-gray-500 items-center shadow-md relative transition-all duration-500 max-w-[80px]",
-        { "!max-w-[0px] p-0": !show }
+        "border-l dark:border-gray-500 items-center shadow-md relative transition-all duration-500 max-w-[80px]",
+        { "!max-w-[0px]": !show }
       )}
     >
       <div
         onClick={() => setShow(!show)}
         className="absolute top-1/2 left-0 bg-slate-400 h-12 w-5 rounded-l-lg grid place-content-center -translate-x-full -translate-y-1/2 cursor-pointer opacity-40 hover:opacity-100 transition"
       >
-        <AiFillCaretLeft
+        <AiFillCaretRight
           className={clsx("rotate-0 transition duration-500", {
             "rotate-180": !show,
           })}
