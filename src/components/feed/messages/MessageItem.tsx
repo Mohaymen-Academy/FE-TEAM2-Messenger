c0377f2ca3247debcafcf1833fee7901b7621de4
mostafagh79 <mostafa.gh.79a@gmail.com>
2023-08-08 10:32:56 +0330

    fix chat message layout

--- src/components/feed/messages/MessageItem.tsx
+++ src/components/feed/messages/MessageItem.tsx
@@ -1,5 +1,6 @@
 import Avatar from "@/components/ui/avatar/Avatar";
 import Paragraph from "@/components/ui/paragraph/Paragraph";
+import clsx from "clsx";
 
 interface MessageItemProps {
   message: string;
@@ -13,14 +14,13 @@ const MessageItem: React.FC<MessageItemProps> = ({ message, sentByCurrentUser })
       flex-row
         p-4
         gap-4
-      hover:bg-white/20
         justify-${sentByCurrentUser ? "start" : "end"}
     `}
     >
       {!sentByCurrentUser && (
         <div className="flex items-end">
           {/* <Avatar size="sm" name={message.sender?.username ? message.sender.username : "USER"} /> */}
-          <Avatar className="mb-3"/>
+          <Avatar className="mb-3" />
         </div>
       )}
       <div className="flex flex-col gap-1 w-full">
@@ -32,7 +32,9 @@ const MessageItem: React.FC<MessageItemProps> = ({ message, sentByCurrentUser })
           justify-${sentByCurrentUser ? "end" : "start"}
         `}
         >
-          {!sentByCurrentUser && <Paragraph className="font-bold text-left">abolfazl</Paragraph>}
+          {!sentByCurrentUser && (
+            <Paragraph className="font-bold text-left">abolfazl</Paragraph>
+          )}
           <Paragraph className="text-sm text-white/70">1402/10/12</Paragraph>
         </div>
         <div
@@ -42,15 +44,14 @@ const MessageItem: React.FC<MessageItemProps> = ({ message, sentByCurrentUser })
           `}
         >
           <div
-            className={`
-              ${sentByCurrentUser ? "bg-blue-600" : "bg-white/40"}
-              px-3
-              py-2
-              rounded-b-3xl
-              rounded-l-3xl
-              max-w-[350px]
-              
-           `}
+            className={clsx(
+              "px-3 py-2  max-w-[350px] rounded-b-3xl bg-blue-600",
+              {
+                
+                "rounded-l-3xl": !sentByCurrentUser,
+                "rounded-r-3xl": sentByCurrentUser,
+              }
+            )}
           >
             <p className="text-white">سلام بر تو ای </p>
           </div>
--- src/components/ui/avatar/Avatar.tsx
+++ src/components/ui/avatar/Avatar.tsx
@@ -3,12 +3,13 @@ import { HTMLAttributes } from "react";
 
 interface AvatarProps extends HTMLAttributes<HTMLDivElement> {
   isOnline: boolean;
+  imgSrc : string;
 }
    
-const Avatar : React.FC<AvatarProps> = ({isOnline , className, ...props}) => {
-  return <div className={merge(className, "h-16 w-16 rounded-full bg-red-600 text-center relative")} {...props}>
-            <div className="absolute w-3 h-3 border rounded-full bg-green-500 top-1 right-2"></div>
-            <img className="w-full h-full rounded-full" />
+const Avatar : React.FC<AvatarProps> = ({isOnline , className, imgSrc, ...props}) => {
+  return <div className={merge(className, "h-12 w-12 text-center relative")} {...props}>
+            <div className="absolute w-3 h-3 border rounded-full bg-green-500 top-1 right-1"></div>
+            <img className="rounded-full " src={imgSrc} />
         </div>;
 };
 
